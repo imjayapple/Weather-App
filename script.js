abc6bbd456f7e35db127a3df46850dce0c213bff
imjayapple <imjayapple@gmail.com>
2024-05-15 11:21:47 -0700

    Symbols appended to temp, humidity and wind speed.

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ async function checkWeather(){
     console.log(data);
 
     document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp;
-    document.querySelector(".humidity").innerHTML = data.main.humidity;
-    document.querySelector(".wind").innerHTML = data.wind.speed;
+    document.querySelector(".temp").innerHTML = data.main.temp + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 }
 
 checkWeather();
