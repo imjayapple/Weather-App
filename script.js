ca707a546e2f5be2df7c9e125eaf71b883a7ded4
imjayapple <imjayapple@gmail.com>
2024-05-15 11:56:52 -0700

    Created new 'weatherIcon' variable, created if-else logic to obtain the weather .json data and match it correctly to an image depicting conditions.

--- script.js
+++ script.js
@@ -6,20 +6,42 @@
 const myApiKey = process.env.secretkey;
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-// process.env.secretkey
-
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${myApiKey}`);
     var data = await response.json();
     console.log(data);
 
+    // Obtain and display all of the necessary weather details
+
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    // Update the image to match the given weather condition
+
+    if(data.weather[0].main == 'Clouds') {
+        weatherIcon.src = "assets/images/clouds.png";
+    }
+    else if(data.weather[0].main == 'Clear'){
+        weatherIcon.src = "assets/images/clear.png";
+    }
+    else if(data.weather[0].main == 'Rain') {
+        weatherIcon.src = "assets/images/rain.png";
+    }
+    else if(data.weather[0].main == 'Drizzle') {
+        weatherIcon.src = "assets/images/drizzle.png";
+    }
+    else if(data.weather[0].main == 'Mist') {
+        weatherIcon.src = "assets/images/mist.png";
+    }
+    else if(data.weather[0].main == 'Snow') {
+        weatherIcon.src = "assets/images/snow.png";
+    }
 }
 
 searchBtn.addEventListener("click", ()=>{
