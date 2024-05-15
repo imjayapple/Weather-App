712269900e92c44174c3f6a7847eee60c24a205c
imjayapple <imjayapple@gmail.com>
2024-05-15 12:25:42 -0700

    Copied & pasted my code into proper & clear if-else blocks, making the error-handling come first, else -> correct input recieved and code executes.

--- script.js
+++ script.js
@@ -12,37 +12,45 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${myApiKey}`);
-    var data = await response.json();
 
-    // Obtain and display all of the necessary weather details
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector("weather").style.display = "none";
+    }else{
+        var data = await response.json();
 
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+        // Obtain and display all of the necessary weather details
+    
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+    
+        // Update the image to match the given weather condition
+    
+        if(data.weather[0].main == 'Clouds') {
+            weatherIcon.src = "assets/images/clouds.png";
+        }
+        else if(data.weather[0].main == 'Clear'){
+            weatherIcon.src = "assets/images/clear.png";
+        }
+        else if(data.weather[0].main == 'Rain') {
+            weatherIcon.src = "assets/images/rain.png";
+        }
+        else if(data.weather[0].main == 'Drizzle') {
+            weatherIcon.src = "assets/images/drizzle.png";
+        }
+        else if(data.weather[0].main == 'Mist') {
+            weatherIcon.src = "assets/images/mist.png";
+        }
+        else if(data.weather[0].main == 'Snow') {
+            weatherIcon.src = "assets/images/snow.png";
+        }
+    
+        document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
 
-    // Update the image to match the given weather condition
-
-    if(data.weather[0].main == 'Clouds') {
-        weatherIcon.src = "assets/images/clouds.png";
     }
-    else if(data.weather[0].main == 'Clear'){
-        weatherIcon.src = "assets/images/clear.png";
-    }
-    else if(data.weather[0].main == 'Rain') {
-        weatherIcon.src = "assets/images/rain.png";
-    }
-    else if(data.weather[0].main == 'Drizzle') {
-        weatherIcon.src = "assets/images/drizzle.png";
-    }
-    else if(data.weather[0].main == 'Mist') {
-        weatherIcon.src = "assets/images/mist.png";
-    }
-    else if(data.weather[0].main == 'Snow') {
-        weatherIcon.src = "assets/images/snow.png";
-    }
-
-    document.querySelector(".weather").style.display = "block";
 }
 
 searchBtn.addEventListener("click", ()=>{
