dd76a00b76ba1606e114763c3d27bae95800106b
imjayapple <imjayapple@gmail.com>
2024-05-15 11:35:16 -0700

    Added two new variables to obtain the city name input from the user, and wrote a simple function to 'plug in' that input into the 'checkWeather()' function.

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+// Utilize your own environment variable to mask your API key, else, process.env.secretkey
+// will not fetch an active API key from OpenWeatherMap and thus the program will not run.
+// Type into the Terminal (I use bash) 'export secretkey=' and paste the API key that was
+// provided to you by OpenWeatherMap.org (You must create an account first to obtain a key).
+
 const myApiKey = process.env.secretkey;
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 // process.env.secretkey
 
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=vancouver";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${myApiKey}`);
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${myApiKey}`);
     var data = await response.json();
     console.log(data);
 
@@ -14,5 +21,9 @@ async function checkWeather(){
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 }
+
+searchBtn.addEventListener("click", ()=>{
+    checkWeather(searchBox.value);
+})
 
 checkWeather();
